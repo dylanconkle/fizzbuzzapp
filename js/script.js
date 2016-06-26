db5741373cc9d6a95b5a65a7983ef5b38343f14a
Dylan Conkle <dconkle@cytracom.com>
2016-06-26 17:00:24 -0500

    added break after fizzbuzz

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 for (var i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
         document.write("fizzbuzz");
+        document.write("<br>");
     } else if (i % 3 == 0) {
         document.write("fizz");
         document.write("<br>");
