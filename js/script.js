bb9df2e6139a33e86604d6aeed06ab782e03d671
Dylan Conkle <dconkle@cytracom.com>
2016-06-26 16:50:19 -0500

    finished Fizzbuzz challenge

---
+++ js/script.js
@@ -0,0 +1,14 @@
+for (var i = 1; i <= 15; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        document.write("fizzbuzz");
+    } else if (i % 3 == 0) {
+        document.write("fizz");
+        document.write("<br>");
+    } else if (i % 5 == 0) {
+        document.write("buzz");
+        document.write("<br>");
+    } else {
+        document.write(i);
+        document.write("<br>");
+    }
+};
